16efd91aaeabba78d6d6d4837b5d25d329644d68
namanxgarg <social.namangarg@gmail.com>
2024-12-22 12:30:08 +0530

    built some controllers

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"golangCrudMysql/pkg/models"
+	"golangCrudMysql/pkg/utils"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marsal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Write(res)
+}
