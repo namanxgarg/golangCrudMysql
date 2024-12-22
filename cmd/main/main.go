6ade0adaf3d5dee377ef90713e8092543cebfcd7
namanxgarg <social.namangarg@gmail.com>
2024-12-22 12:56:37 +0530

    corrected some small errors

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"golangCrudMysql/pkg/routes"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main(){
-	r:=mux.NewRouter()
+func main() {
+	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":9010", r))
